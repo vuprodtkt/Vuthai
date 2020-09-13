4b4df3a13142ac6001a6ad74c8fea0d57e3f27bd
vuprodtkt <68179122+vuprodtkt@users.noreply.github.com>
2020-09-13 11:23:39 +0700

    Update thamgia.php

--- thamgia.php
+++ thamgia.php
@@ -106,8 +106,8 @@ function ketnoi($userid,$gioitinh) { //tìm người chát
           "template_type":"generic",
           "elements":[
             {
-              "title":"Đang thả câu...",
-              "subtitle":"Đợi xíu BOT đang tìm một cá nữ cho bạn (👩)"
+              "title":"Đang tìm người...",
+              "subtitle":"Đợi xíu BOT đang tìm một bạn nữ cho bạn (👩)"
             }
           ]
         }
@@ -126,8 +126,8 @@ function ketnoi($userid,$gioitinh) { //tìm người chát
           "template_type":"generic",
           "elements":[
             {
-              "title":"Đang thả câu...",
-              "subtitle":"Đợi xíu BOT đang tìm một cá nam cho bạn (👱)"
+              "title":"Đang tìm người...",
+              "subtitle":"Đợi xíu BOT đang tìm một bạn nam cho bạn (👱)"
             }
           ]
         }
@@ -146,8 +146,8 @@ function ketnoi($userid,$gioitinh) { //tìm người chát
           "template_type":"generic",
           "elements":[
             {
-              "title":"Đang thả câu...",
-              "subtitle":"Đợi xíu BOT đang tìm một cá ẩn giới tính giống bạn (👤)"
+              "title":"Đang tìm người...",
+              "subtitle":"Đợi xíu BOT đang tìm một bạn ẩn giới tính giống bạn (👤)"
             }
           ]
         }
@@ -206,8 +206,8 @@ echo'{
           "template_type":"generic",
           "elements":[
             {
-              "title":"Đang thả câu...",
-              "subtitle":"Chưa có cá nào dính thính đâu. Bạn chờ chút nhé! "
+              "title":"Đang tìm người...",
+              "subtitle":"Chưa tìm được người đâu. Bạn chờ chút nhé! "
             }
           ]
         }
@@ -228,7 +228,7 @@ echo'{
           "elements":[
             {
               "title":"Cảnh báo",
-              "subtitle":"Bạn đang được kết nối với cá rồi ! Hãy gõ \'End\' để thoát"
+              "subtitle":"Bạn đang được kết nối với người lạ rồi! Hãy gõ \'End\' để thoát"
             }
           ]
         }
